f5429b9c426d6316a2e573f47e58a91e802433d6
Ksusha1996 <123461531+Ksusha1996@users.noreply.github.com>
2023-08-06 19:05:10 +0000

    tab 25

---
+++ tab 25/Program.cs
@@ -0,0 +1,37 @@
+﻿// ва числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число A: ");
+        int A = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число B: ");
+        int B = int.Parse(Console.ReadLine());
+
+        int result = Power(A, B);
+        Console.WriteLine($"Результат: {result}");
+    }
+
+    static int Power(int baseNumber, int exponent)
+    {
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+}
+
+
+
+
+
+
+
